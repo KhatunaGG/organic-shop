b0d5f03136c2395cc192b6508248ab8f005410f6
KG <khatunaggg12345@gmail.com>
2024-06-18 21:20:29 +0400

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import { Header, Nav, Products, SignSection } from "./components/index";
 
 export default function Home() {
   return (
-    <main className="">
-  
+    <main className=" w-full h-full ">
+      <SignSection />
+      <Nav />
+      <Header />
+      <section className="max-w-screen-xl  mx-auto  px-[3%] lg:px-[7%] ">
+        <Products />
+      </section>
     </main>
   );
 }
